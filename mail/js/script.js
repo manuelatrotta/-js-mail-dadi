99b69ce2886a3b8c8012d2b2dc6b88cf4c80149e
manuelatrotta <manuelat88@hotmail.it>
2020-01-07 16:32:41 +0100

    responso all'utente

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,3 +4,17 @@ var listaEmails = ['manuelat88@hotmail.it', 'sampey@yahoo.it', 'carlo_b@kinko.co
 //chiedere email all'utente
 var emailUtente = (prompt('inserisci la tua email per verificare se sei tra i partecipanti'));
 console.log(emailUtente);
+var emailPresente = false;
+for (var i=0; i<listaEmails.length; i++){
+  var emails = listaEmails[i];
+  if(emailUtente==emails){
+    emailPresente = true;
+  }
+}
+console.log(emailPresente);
+if(emailPresente==false){
+  console.log('email non presente');
+}
+else{
+  console.log('email presente');
+}
