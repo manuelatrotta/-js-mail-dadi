3ded1d2948cc702305b440db35ba0425766e036d
manuelatrotta <manuelat88@hotmail.it>
2020-01-07 17:00:11 +0100

    inserimento numeri del dado e lancio per i due giocatori

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,5 +1,12 @@
 alert('Are you ready?');
 //lista numeri dadi da 1 a 6
+var numeriDado = [1,2,3,4,5,6];
 // giocatore 1 lancio dado con random da 1 a 6
+alert('turno Player 1');
+var numeroPlayer1 = Math.floor(Math.random() * 6) + 1;
+console.log(numeroPlayer1);
 //giocatore 2 lancio dado con random da 1 a 6
+alert('turno Player2');
+var numeroPlayer2 = Math.floor(Math.random() * 6) + 1;
+console.log(numeroPlayer2);
 //confronto tra i due numeri usciti e vincita numero più grande
