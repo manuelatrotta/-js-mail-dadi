b7252d463419cd8257da46b27eee796465fd36d8
manuelatrotta <manuelat88@hotmail.it>
2020-01-07 16:36:12 +0100

    collegamento js con html per gioco dadi

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,12 +5,14 @@ var listaEmails = ['manuelat88@hotmail.it', 'sampey@yahoo.it', 'carlo_b@kinko.co
 var emailUtente = (prompt('inserisci la tua email per verificare se sei tra i partecipanti'));
 console.log(emailUtente);
 var emailPresente = false;
+//verificare presenza email dell'utente nella lista listaemails
 for (var i=0; i<listaEmails.length; i++){
   var emails = listaEmails[i];
   if(emailUtente==emails){
     emailPresente = true;
   }
 }
+//comunicare il responso all'utente
 console.log(emailPresente);
 if(emailPresente==false){
   console.log('email non presente');
