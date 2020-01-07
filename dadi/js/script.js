9465b7de95e61c316b11809ab724ae3565039cc0
manuelatrotta <manuelat88@hotmail.it>
2020-01-07 16:38:23 +0100

    schema procedimento

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,5 @@
 alert('Are you ready?');
+//lista numeri dadi da 1 a 6
+// giocatore 1 lancio dado con random da 1 a 6
+//giocatore 2 lancio dado con random da 1 a 6
+//confronto tra i due numeri usciti e vincita numero più grande
