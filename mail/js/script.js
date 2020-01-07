097bf829d78faa1502e2b57eba402ccb1e99d2a3
manuelatrotta <manuelat88@hotmail.it>
2020-01-07 16:02:09 +0100

    creazione lista e richiesta email utente

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,6 @@
 alert('welcome');
+//creare lista di emails
+var listaEmails = ['manuelat88@hotmail.it', 'sampey@yahoo.it', 'carlo_b@kinko.com', 'ashey@tiscali.it'];
+//chiedere email all'utente
+var emailUtente = (prompt('inserisci la tua email per verificare se sei tra i partecipanti'));
+console.log(emailUtente);
